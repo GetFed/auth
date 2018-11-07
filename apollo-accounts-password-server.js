8e1ff50094287f0cc35cfab23cbb450bfc1a2108
Greg <gregt87@gmail.com>
2018-11-07 18:00:20 -0500

    re-init

---
+++ apollo-accounts-password-server.js
@@ -0,0 +1,79 @@
+import { AccountsServer } from '@accounts/server'
+import { AccountsPassword } from '@accounts/password'
+import MongoDBInterface from '@accounts/mongo'
+import { createAccountsGraphQL } from '@accounts/graphql-api'
+import { DatabaseManager } from '@accounts/database-manager'
+import { randomBytes } from 'crypto'
+import { defaultsDeep } from 'lodash'
+
+export { accountsContext } from '@accounts/graphql-api'
+
+const METEOR_ID_LENGTH = 17
+const idProvider = () =>
+  randomBytes(30)
+    .toString('base64')
+    .replace(/[\W_]+/g, '')
+    .substr(0, METEOR_ID_LENGTH)
+
+const dateProvider = date => (date ? date : new Date())
+
+const onLoginHooks = []
+export const onLogin = hook => onLoginHooks.push(hook)
+const callOnLoginHooks = info => onLoginHooks.forEach(hook => hook(info))
+
+const onCreateUserHooks = []
+export const onCreateUser = hook => onCreateUserHooks.push(hook)
+const callOnCreateUserHooks = user =>
+  onCreateUserHooks.forEach(hook => hook(user))
+
+const defaultOptions = {
+  tokenSecret: 'insecure',
+  tokenConfigs: {
+    refreshToken: {
+      expiresIn: '90d'
+    }
+  },
+  siteUrl: 'http://localhost:3000'
+}
+
+export const createApolloAccounts = ({ db, ...givenOptions }) => {
+  if (!db) {
+    console.error('createApolloAccounts: db is a required parameter')
+  }
+  if (!givenOptions.tokenSecret) {
+    console.log(
+      'Warning: Must provide a tokenSecret (long random string) to createApolloAccounts()'
+    )
+  }
+
+  const mongoStorage = new MongoDBInterface(db, {
+    convertUserIdToMongoObjectId: false,
+    convertSessionIdToMongoObjectId: false,
+    idProvider,
+    dateProvider
+  })
+
+  const dbManager = new DatabaseManager({
+    sessionStorage: mongoStorage,
+    userStorage: mongoStorage
+  })
+
+  const options = defaultsDeep(givenOptions, defaultOptions)
+
+  const accountsServer = new AccountsServer(
+    { db: dbManager, ...options },
+    {
+      password: new AccountsPassword({ passwordHashAlgorithm: '' })
+    }
+  )
+
+  // full list of hooks:
+  // https://github.com/accounts-js/accounts/blob/master/packages/server/src/utils/server-hooks.ts
+  accountsServer.on('LoginSuccess', callOnLoginHooks)
+  accountsServer.on('CreateUserSuccess', callOnCreateUserHooks)
+
+  return {
+    accountsServer,
+    graphQL:createAccountsGraphQL(accountsServer, { extend: true })
+  }
+}
---
+++ server.js
@@ -0,0 +1,89 @@
+import { ApolloServer, makeExecutableSchema } from 'apollo-server'
+import {
+  createApolloAccounts,
+  accountsContext,
+  onLogin,
+  onCreateUser
+} from './apollo-accounts-password-server'
+import { merge } from 'lodash'
+import mongodb from 'mongodb'
+
+onLogin(info => console.log('onLogin', info))
+onCreateUser(user => console.log('onCreateUser', user))
+
+const start = async () => {
+  const client = await mongodb.MongoClient.connect(
+    process.env.MONGO_URL || 'mongodb://127.0.0.1:3004/meteor',
+    { useNewUrlParser: true }
+  )
+
+  const db = client.db()
+
+  const accounts = createApolloAccounts({
+    db,
+    tokenSecret: process.env.TOKEN_SECRET || "ABCDEFG", // not abcd
+    siteUrl:
+      process.env.NODE_ENV === 'production'
+        ? 'https://myapp.com'
+        : 'http://localhost:3000'
+  })
+
+  const typeDefs = `
+  type PrivateType @auth {
+    field: String
+  }
+
+  type Query {
+    publicField: String
+    privateField: String @auth
+    privateType: PrivateType
+    adminField: String @auth
+  }
+
+  type Mutation {
+    _: String
+  }
+
+  extend type User {
+    firstName: String
+  }
+  `
+
+  const resolvers = {
+    Query: {
+      publicField: () => 'public',
+      privateField: () => 'private',
+      privateType: () => ({
+        field: () => 'private'
+      }),
+      adminField: (root, args, context) => {
+        if (context.user.isAdmin) {
+          return 'admin field'
+        }
+      }
+    },
+    User: {
+      firstName: () => 'first'
+    }
+  }
+
+  const schema = makeExecutableSchema({
+    typeDefs: [typeDefs, accounts.graphQL.typeDefs],
+    resolvers: merge(accounts.graphQL.resolvers, resolvers),
+    schemaDirectives: {
+      ...accounts.graphQL.schemaDirectives
+    }
+  })
+
+  const server = new ApolloServer({
+    schema,
+    context: ({ req }) => accountsContext(req, { accountsServer: accounts.accountsServer }),
+    formatError: e => console.log(e) || e
+  })
+
+  server.listen(4000).then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`)
+  })
+}
+
+start()
